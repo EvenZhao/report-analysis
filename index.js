85430cc001f5430fdf2a418b07cc4159b4b48d80
Even Zhao <EvenZhao@Evens-MacBook-Air.local>
2019-09-03 14:56:27 +0800

    feat: 加入echart

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cheerio = require('cheerio');
+
 const http = require('http');
 const fs = require('fs');
 const Koa = require('koa');
@@ -22,6 +24,43 @@ app.use(async (ctx, next) => {
 	} else if (request.url === '/src/views/index.jsx' && request.method === 'GET') {
 		ctx.set({ 'Content-Type': 'application/x-javascript' });
 		ctx.response.body = fs.readFileSync('./src/views/index.jsx');
+	} else if (request.url.includes('getCompany') && request.method === 'GET') {
+		let params = request.url;
+		params = params.substring(params.indexOf('?') + 1);
+		const arr = params.split('&');
+		params = {};
+		arr.forEach(item => {
+			const _arr = item.split('=');
+			/* eslint-disable */
+			params[_arr[0]] = _arr[1];
+			/* eslint-enable */
+		});
+		await new Promise((resolve, reject) => {
+			http.get(`http://quotes.money.163.com/f10/zycwzb_${params.word},year.html`, (res) => {
+				res.setEncoding('utf8');
+				let rawData = '';
+				res.on('data', (chunk) => { rawData += chunk; });
+				res.on('end', async () => {
+					try {
+						const $ = cheerio.load(rawData);
+						let fragment = $('.col_r table tr').eq(10).html();
+						let datas = $('.col_r table tr').html();
+						/* eslint-disable */
+						fragment = fragment.replace(/,/g, '').replace(/\<td\>/g, '').split('</td>').slice(0, -1);
+						datas = datas.replace(/,/g, '').replace(/\<th\>/g, '').split('</th>').slice(0, -1);
+						/* eslint-enable */
+						ctx.body = {
+							fragment,
+							datas,
+						};
+						resolve();
+					} catch (e) {
+						console.error(e.message);
+						reject();
+					}
+				});
+			});
+		});
 	} else if (request.url.includes('getStock') && request.method === 'GET') {
 		let params = request.url;
 		params = params.substring(params.indexOf('?') + 1);
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -2,23 +2,68 @@ class Input extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: 6,
+			value: '',
 			list: [],
 		}
 	}
 	handleBlur() {
-		function createXHR(){
-			if (typeof XMLHttpRequest != "undefined"){
+		if (this.state.value.trim() === '') {
+			this.setState({ list: [] });
+		} else {
+			function createXHR() {
+				if (typeof XMLHttpRequest != "undefined") {
+					return new XMLHttpRequest();
+				} else if (typeof ActiveXObject != "undefined") {
+					if (typeof arguments.callee.activeXString != "string") {
+						var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp"], i, len;
+						for (i = 0, len = versions.length; i < len; i++) {
+							try {
+								new ActiveXObject(versions[i]);
+								arguments.callee.activeXString = versions[i];
+								break;
+							} catch (ex) {
+								//跳过
+							}
+						}
+					}
+					return new ActiveXObject(arguments.callee.activeXString);
+				} else {
+					throw new Error("No XHR object available.");
+				}
+			}
+			var xhr = createXHR();
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState == 4) {
+					if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+						const value = JSON.parse(xhr.responseText);
+						this.setState({ list: value });
+					} else {
+						alert("Request was unsuccessful: " + xhr.status);
+					}
+				}
+			};
+			xhr.open('get', `http://localhost:8080/getStock?word=${this.state.value}`, true);
+			xhr.send(null);
+		}
+	}
+	handleChange(e) {
+		this.setState({ value: e.target.value })
+	}
+	handleClick(index) {
+		const symbol = this.state.list[index].symbol;
+		console.log(symbol);
+		function createXHR() {
+			if (typeof XMLHttpRequest != "undefined") {
 				return new XMLHttpRequest();
-			} else if (typeof ActiveXObject != "undefined"){
-				if (typeof arguments.callee.activeXString != "string"){
-					var versions = [ "MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0","MSXML2.XMLHttp"], i, len;
-					for (i=0,len=versions.length; i < len; i++){
+			} else if (typeof ActiveXObject != "undefined") {
+				if (typeof arguments.callee.activeXString != "string") {
+					var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp"], i, len;
+					for (i = 0, len = versions.length; i < len; i++) {
 						try {
 							new ActiveXObject(versions[i]);
 							arguments.callee.activeXString = versions[i];
 							break;
-						} catch (ex){
+						} catch (ex) {
 							//跳过
 						}
 					}
@@ -31,20 +76,35 @@ class Input extends React.Component {
 		var xhr = createXHR();
 		xhr.onreadystatechange = () => {
 			if (xhr.readyState == 4) {
+
 				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-					const value = JSON.parse(xhr.responseText);
-					console.log(value);
-					this.setState({ list: value });
+					const resDatas = JSON.parse(xhr.responseText);
+					const myChart = echarts.init(document.getElementById('chart'));
+					console.log(typeof resDatas.datas);
+					// 指定图表的配置项和数据
+					const option = {
+						xAxis: {
+							// type: 'datas',
+							data: resDatas.datas
+						},
+						yAxis: {
+							// type: 'value'
+						},
+						series: [{
+							data: resDatas.fragment,
+							type: 'line'
+						}]
+					};
+					// 使用刚指定的配置项和数据显示图表。
+					myChart.setOption(option);
 				} else {
 					alert("Request was unsuccessful: " + xhr.status);
 				}
 			}
 		};
-		xhr.open('get', `http://localhost:8080/getStock?word=${this.state.value}`, true);
+		xhr.open('get', `http://localhost:8080/getCompany?word=${symbol}`, true);
 		xhr.send(null);
-	}
-	handleChange(e) {
-		this.setState({ value: e.target.value })
+
 	}
 	render() {
 		return (
@@ -55,10 +115,18 @@ class Input extends React.Component {
 					onChange={(e) => this.handleChange(e)}
 				/>
 				<p>
-					{ this.state.list.map((item) => {
-						return item.name;
-					}) }
+					{this.state.list.map((item, index) => {
+						return (
+							<button
+								key={index}
+								onClick={() => this.handleClick(index)}
+							>
+								{item.name}
+							</button>
+						)
+					})}
 				</p>
+				<div id="chart" style={{width: 1000,height:400}}></div>
 			</div>
 		);
 	}
@@ -68,3 +136,6 @@ class Input extends React.Component {
 ReactDOM.render(< Input />,
 	document.getElementById('app')
 );
+
+
+// 每年净利润变化
